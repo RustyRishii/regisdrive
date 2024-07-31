6275e3a2094eb4c070accc3a670d886cbff5caf2
Rishikesh-278 <rishikeshbidkar04@gmail.com>
2024-07-31 08:18:05 +0530

    3rd test

--- app/api/auth/[...nextauth]/[...nextauth].ts
+++ app/api/auth/[...nextauth]/[...nextauth].ts
@@ -1,22 +1,14 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+//import GithubProvider from "next-auth/providers/github"
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-  pages: {
-    signIn: "/dashboard",
-  },
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ]
+}
 
-  callbacks: {
-    async session({ session, token, user }) {
-      //session.user.username = session.user.name
-      //session.user.uid = token.sub;
-      return session;
-    },
-  },
-});
+export default NextAuth(authOptions)
